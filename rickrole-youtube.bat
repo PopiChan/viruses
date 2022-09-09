7f619c7f4c267cef2ab202cabc116b79ad300edd
Asaf Meizner <asafmeizner@gmail.com>
2022-09-09 22:57:16 +0300

    Added rickrole-youtube

---
+++ rickrole-youtube.bat
@@ -0,0 +1,17 @@
+@if (@a==@b) @end /*
+
+:: batch portion
+
+@ECHO OFF
+
+start " " https://www.youtube.com/watch?v=QtBDL8EiNZo
+
+cscript /e:jscript "%~f0"
+
+
+:: JScript portion */
+
+var shl = new ActiveXObject("WScript.Shell");
+for (var i=0; i<50; i++) {
+    shl.SendKeys(String.fromCharCode(0xAF));
+}
